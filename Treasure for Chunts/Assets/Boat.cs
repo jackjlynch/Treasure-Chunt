fb26c92f0f7a5dee30dd835c874c44f45bbb7825
Jack Lynch <jacklynch@wharnal.com>
2015-06-19 19:39:23 -0400

    added boat and background

---
+++ Treasure for Chunts/Assets/Boat.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boat : MonoBehaviour {
+    Rigidbody2D rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.AddForce(new Vector2(1, 0), ForceMode2D.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Treasure for Chunts/Assets/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+    GameObject boat;
+
+	// Use this for initialization
+	void Start () {
+        boat = GameObject.Find("BOET");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(boat.transform.position.x, boat.transform.position.y, -10);
+	}
+}

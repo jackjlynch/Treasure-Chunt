cff32f389fff83a3812b6e0c92209ce2ef5c9724
Jack Lynch <jacklynch@wharnal.com>
2015-06-19 17:27:20 -0400

    test movement

---
+++ Treasure for Chunts/Assets/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject hans = GameObject.Find("hans");
+        transform.position = new Vector3(hans.transform.position.x, hans.transform.position.y, -10);
+	
+	}
+}
--- Treasure for Chunts/Assets/NewBehaviourScript.cs
+++ Treasure for Chunts/Assets/NewBehaviourScript.cs
@@ -6,14 +6,16 @@ public class NewBehaviourScript : MonoBehaviour {
 	DateTime timeOfLastClick;
 	TimeSpan desiredInterval;
 	TimeSpan gracePeriod;
-	Vector3 distance;
+    Vector3 distance;
+    Rigidbody2D rigidbody;
 
 	// Use this for initialization
 	void Start () {
 		timeOfLastClick = DateTime.Now;
-		desiredInterval = new TimeSpan (0, 0, 0, 0, 500);
-		gracePeriod = new TimeSpan (0, 0, 0, 0, 100);
-		distance = new Vector3 (1, 0);
+		desiredInterval = new TimeSpan(0, 0, 0, 0, 500);
+		gracePeriod = new TimeSpan(0, 0, 0, 0, 300);
+		distance = new Vector3(1, 0);
+        rigidbody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
@@ -21,8 +23,9 @@ public class NewBehaviourScript : MonoBehaviour {
 		if(Input.GetKeyDown("space")) {
 			TimeSpan difference = DateTime.Now.Subtract(timeOfLastClick);
 			if(difference < desiredInterval + gracePeriod && difference > desiredInterval - gracePeriod) {
-				transform.position = transform.position + distance;
-				Debug.Log ("triggered");
+                //transform.position = transform.position + distance;
+                rigidbody.AddForce(distance, ForceMode2D.Impulse);
+				Debug.Log("triggered");
 			}
 			timeOfLastClick = DateTime.Now;
 		}
